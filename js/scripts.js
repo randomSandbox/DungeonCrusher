ed3705119bf2e1be82a42aac06c22eae4ea96144
Florent Augereau <faugereau@modedemploi.fr>
2023-03-24 23:01:31 +0100

    do you think its better

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
-const words=['ZHJhYWRvcg==','dm9pZA==','YXZlbnR1cmU='];
-let cpt = 0;
-let message = "mot clé incorrect";
-var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
-n = 0;
-$(document).keydown(function (e) {
-    if (e.keyCode === k[n++]) {
-        if (n === k.length) {
-            $('.mainlogo').attr('src', "img/qrcode.png");
-            n = 0;
-            return false;
-        }
-    }
-    else {
-        n = 0;
-    }
-});
-
-console.log(`
+const words=["ZHJhYWRvcg==","dm9pZA==","YXZlbnR1cmU="];let cpt=0,message="mot cl\xe9 incorrect";var k=[38,38,40,40,37,39,37,39,66,65],n=0;$(document).keydown(function(s){if(s.keyCode===k[n++]){if(n===k.length)return $(".mainlogo").attr("src","img/qrcode.png"),n=0,!1}else n=0}),console.log(`
 ████████▄     ▄████████    ▄████████    ▄████████ ████████▄   ▄██████▄     ▄████████ 
 ███   ▀███   ███    ███   ███    ███   ███    ███ ███   ▀███ ███    ███   ███    ███ 
 ███    ███   ███    ███   ███    ███   ███    ███ ███    ███ ███    ███   ███    ███ 
@@ -26,25 +8,4 @@ console.log(`
 ███   ▄███   ███    ███   ███    ███   ███    ███ ███   ▄███ ███    ███   ███    ███ 
 ████████▀    ███    ███   ███    █▀    ███    █▀  ████████▀   ▀██████▀    ███    ███ 
              ███    ███                                                   ███    ███ 
-`);
-
-$('.js-submit-password').on('click', function(){
-	cpt++;
-	if (cpt >= 100) {
-		message = "</p><p class='success'>T'es vraiment un acharné du bocal toi.</p><p> Voila un mot de passe de consolation : TAPER";
-	}
-	else if(cpt>=20) {
-		message = "Non mais là tu tentes des mots au pif en vrai.";
-	}
-	else if(cpt >= 10){
-		message = "ça fait beaucoup de mot clé incorrect là non ?";
-	}
-	let text = $('.mot-cle').val().toLowerCase();
-	let final = btoa(text);
-	if(words.includes(final)){
-		$('.js-result').html('<p class="success">Félicitations tu as trouvé un mots clés gagnants.</p><p>Le mot de passe est : <span class="bold">' + btoa(final) + '</span></p>');
-	}
-	else {
-		$('.js-result').html('<p class="error">' + message + '</p>');
-	}
-});
+`),$(".js-submit-password").on("click",function(){++cpt>=100?message="</p><p class='success'>T'es vraiment un acharn\xe9 du bocal toi.</p><p> Voila un mot de passe de consolation : "+atob("VEFQRVI="):cpt>=20?message="Non mais l\xe0 tu tentes des mots au pif en vrai.":cpt>=10&&(message="\xe7a fait beaucoup de mot cl\xe9 incorrect l\xe0 non ?");let s=$(".mot-cle").val().toLowerCase(),e=btoa(s);words.includes(e)?$(".js-result").html('<p class="success">F\xe9licitations tu as trouv\xe9 un mot cl\xe9 gagnant.</p><p>Le mot de passe est : <span class="bold">'+btoa(e)+"</span></p>"):$(".js-result").html('<p class="error">'+message+"</p>")});
